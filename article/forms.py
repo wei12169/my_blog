4a5f53a691f949077447b456a26ea183ce631b3b
wei12169 <1191763804@qq.com>
2021-12-03 10:43:11 +0800

    Basic functions of the article

---
+++ article/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.db import models
+from django.forms import fields
+from .models import ArticlePost
+
+class ArticlePostForm(forms.ModelForm):
+    class Meta:
+        #指明数据模型来源
+        model = ArticlePost
+        #定义表单包含的字段
+        fields = ('title', 'body')
---
+++ article/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+#博客文章数据类型
+class ArticlePost(models.Model):
+    #文章作者。参数on_delete用于指定数据删除方式
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #文章标题。models.CharField为字符串字段，用于保存比较短的字符串
+    title = models.CharField(max_length=100)
+
+    #文章正文。保存大量文本使用TextField
+    body = models.TextField()
+
+    #文章创建时间。参数default=timezone.now指定在创建数据时默认写入当前时间
+    created = models.DateTimeField(default=timezone.now)
+
+    #文章更新时间。参数auto_now=True指定每次数据更新时自动写入当前时间
+    updated = models.DateTimeField(auto_now=True)
+
+    #内部类class Meta用于给model定义元数据
+    class Meta:
+        #ordering指定模型返回的数据的排列顺序
+        # '-created'表明数据应以倒序排列
+        ordering = ('-created',)
+
+    #函数__str__定义当调用对象的str()方法时返回值内容
+    def __str__(self) -> str:
+        #将文章标题返回
+        return self.title
