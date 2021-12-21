789e9ef20905c2e15aa3dc6b4a151f28a8bc30b1
wei12169 <1191763804@qq.com>
2021-12-21 18:21:08 +0800

    add func of the anchor positioning

--- comment/views.py
+++ comment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from notifications.signals import notify
 from django.contrib.auth.models import User
 
@@ -38,7 +38,8 @@ def post_comment(request, article_id, parent_comment_id=None):
                         target=article,
                         action_object=new_comment,
                     )
-                return HttpResponse('200 OK')
+                # return HttpResponse('200 OK')
+                return JsonResponse({"code": "200 OK", "new_comment_id": new_comment.id})
 
             new_comment.save()
 
@@ -51,8 +52,9 @@ def post_comment(request, article_id, parent_comment_id=None):
                     target=article,
                     action_object=new_comment,
                 )
-
-            return redirect(article)
+            #添加锚点
+            redirect_url = article.get_absolute_url()+'#comment_elem_'+str(new_comment.id)
+            return redirect(redirect_url)
         else:
             return HttpResponse('表单有误，请重新填写。')
     #处理GET请求
